9c88b6f4d668760b55776ef6a4b217ad67c946bc
terrynuechterlein <terrynuechterlein@gmail.com>
2024-02-08 05:20:19 -0500

    signup/login functionality

--- src/app/AuthPage/page.jsx
+++ src/app/AuthPage/page.jsx
@@ -1,15 +1,65 @@
-'use client';
-import React, { useState } from "react";
+"use client";
+import React, {useState} from "react";
 import "./authpage.css";
-import {BlackHoverBtn, WhiteToBlackBtn, WhiteOutlineBtn}  from "../../Components/Button";
+import {
+  BlackHoverBtn,
+  WhiteToBlackBtn,
+  WhiteOutlineBtn,
+} from "../../Components/Button";
 import Image from "next/image";
 import Link from "next/link";
-import {useRouter} from 'next/navigation';
-import StatefulTabs from "../../Components/StatefulTabs"; 
+import StatefulTabs from "../../Components/StatefulTabs";
 
 const AuthPage = () => {
-  const [activeForm, setActiveForm] = useState('signUp');
-  const router = useRouter();
+  const [activeForm, setActiveForm] = useState("signUp");
+
+  const handleSignup = async (event) => {
+    event.preventDefault();
+    const userData = {
+      email: event.target.email.value,
+      username: event.target.username.value,
+      password: event.target.password.value,
+    };
+
+    const response = await fetch("http://localhost:8081/api/v1/user/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(userData),
+    });
+
+    if (response.ok) {
+      console.log("Signup successful");
+      setActiveForm("signIn");
+    } else {
+      console.error("Signup failed");
+    }
+  };
+
+  const handleSignin = async (event) => {
+    event.preventDefault();
+    const userData = {
+      email: event.target.email.value,
+      password: event.target.password.value,
+    };
+
+    const response = await fetch("http://localhost:8081/api/v1/user/signin", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(userData),
+    });
+
+    if (response.ok) {
+      console.log("Signin successful");
+      // Redirect to Dashboard page
+      window.location.href = "/Dashboard"; 
+    } else {
+      console.error("Signin failed");
+    }
+  };
 
   return (
     <div className="container_auth">
@@ -17,36 +67,48 @@ const AuthPage = () => {
       <div className="auth__logo">
         <Image src="/assets/logo3.png" alt="Logo" width={100} height={100} />
       </div>
-      
-      <div className={activeForm === 'signUp' ? "auth__form form__signup" : "auth__form form__signin"}>
+
+      <div
+        className={
+          activeForm === "signUp"
+            ? "auth__form form__signup"
+            : "auth__form form__signin"
+        }>
         <StatefulTabs activeForm={activeForm} setActiveForm={setActiveForm} />
 
-        {activeForm === 'signUp' ? (
-          <div className="form__input-group">
-            <input type="email" placeholder="Email" />
-            <input type="text" placeholder="Username" />
-            <input type="password" placeholder="Password" />
-            <input type="password" placeholder="Confirm Password" />
-          </div>
+        {activeForm === "signUp" ? (
+          <form onSubmit={handleSignup}>
+            <div className="form__input-group">
+              <input type="email" name="email" placeholder="Email" />
+              <input type="text" name="username" placeholder="Username" />
+              <input type="password" name="password" placeholder="Password" />
+              <input
+                type="password"
+                name="confirmPassword"
+                placeholder="Confirm Password"
+              />
+              <div className="form__button">
+                <WhiteToBlackBtn type="submit">Create Account</WhiteToBlackBtn>
+              </div>
+            </div>
+          </form>
         ) : (
-          <div className="form__input-group">
-            <input type="text" placeholder="Username" />
-            <input type="password" placeholder="Password" />
-          </div>
-        )}
-        {activeForm === 'signUp' ? (
-          <div className="form__button">
-            <WhiteToBlackBtn type="button" onClick={() => setActiveForm('signIn')} className={activeForm === 'signIn' ? 'active' : ''}>Create Account</WhiteToBlackBtn>
-          </div>
-        ) : (
-          <div className="form__button">
-            <WhiteToBlackBtn type="button" onClick={() => router.push('/Dashboard')}>Login</WhiteToBlackBtn>
-          </div>
-        )}
-        {activeForm === 'signIn' && (
-          <div className="form__forgot-text">
-            <Link href="/forgot-password" className="forgot-text__forgot-link">Forgot password?</Link>
-          </div>
+          <form onSubmit={handleSignin}>
+            <div className="form__input-group">
+              <input type="email" name="email" placeholder="Email" />
+              <input type="password" name="password" placeholder="Password" />
+              <div className="form__button">
+                <WhiteToBlackBtn type="submit">Login</WhiteToBlackBtn>
+              </div>
+              <div className="form__forgot-text">
+                <Link
+                  href="/forgot-password"
+                  className="forgot-text__forgot-link">
+                  Forgot password?
+                </Link>
+              </div>
+            </div>
+          </form>
         )}
       </div>
     </div>
